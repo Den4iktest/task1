1f25139bfa3997d2cd01e1d5f5a678fcd7babf76
Den4iktest <iqden4ik@gmail.com>
2021-12-15 15:56:22 +0200

    ступор

--- create.php
+++ create.php
@@ -5,29 +5,35 @@ include('functions.php');
 $pdo = new PDO("mysql:host=localhost;dbname=marlin;charset=utf8",'root','');
 
 
-// $name = $_POST['name'];
-// $job = $_POST['job'];
-// $phone = $_POST['phone'];
-// $adress = $_POST['adress'];
+$name = $_POST['name'];
+$job = $_POST['job'];
+$phone = $_POST['phone'];
+$adress = $_POST['adress'];
 $email = $_POST['email'];
 $password = $_POST['password'];
-// $status = $_POST['status'];
-// $file = $_POST['file'];
-// redirect_to('create_user');
+$status = $_POST['status'];
+$file = $_POST['file'];
+
+// var_dump($_POST);
 
 
 if(get_user_by_email($email)){
     set_flash_message('danger','Такой email уже зарегестрирован');  
     redirect_to('create_user');
-}  else{
-$sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+}else {
+$sql = "INSERT INTO test (email, password, username, job, phone, adress, status) VALUES (:email, :password, :username, :job, :phone, :adress, :status)";
 $statement = $pdo->prepare($sql);
 $statement->execute([
     'email' => $email,
-    'password' => password_hash($password,PASSWORD_DEFAULT)
+    'password' => password_hash($password,PASSWORD_DEFAULT),
+    'username' => $name,
+    'job' => $job,
+    'phone' => $phone,
+    'adress' => $adress,
+    'status' => $status
+    
 ]);
 set_flash_message('success','Пользователь успешно добавлен');
 redirect_to('create_user');
-}
-
+} 
 ?>
